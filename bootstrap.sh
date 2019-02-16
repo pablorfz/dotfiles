f072d7450ce468c56e0754bf4e51a7f06f3c8601
Pablo Rodriguez <pablo.rodriguez@github.com>
2019-02-16 14:06:36 +0000

    Basic apps

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+
+brew cask install google-chrome
+brew cask install slack
+
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+brew cask install visual-studio-code
+brew cask install intellij-idea
+brew cask install datagrip
+brew cask install docker
